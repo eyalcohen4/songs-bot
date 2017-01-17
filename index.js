f59b80b16d1a7e72d72c1512ae756dca20bbafb9
eyalcohen4 <Eyalcohen4.ec@gmail.com>
2017-01-18 00:31:09 +0200

    Start to handle user request for names

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-const api = require('./api');
+const api       = require('./api');
+const encodeUrl = require('encodeurl');
 
 module.exports = function(bp) {
-  bp.middlewares.load()
+  bp.middlewares.load();
 
-   bp.hear('שלום', event => { // Capture messages that are 'hello'
-    // Respond to the user with 'Hello, world!'
-    api.sendRequest('http://localhost:3010/api/songs/lyrics/%D7%90%D7%97%D7%A8%D7%AA').then(data => {
-        let lyrics;
-        data = JSON.parse(data);
+   bp.hear(/[\u0590-\u05FF]/, event => {
+    let term = encodeUrl(event.text);
+    console.log(term);
+    api.sendRequest(`http://localhost:3001/api/songs/name/${term}`).then(data => {
         console.log(data);
+        data = JSON.parse(data);
+
         data.map(function (item) {
-            // lyrics = text.slice(1, 15)
-            // console.log(lyrics);
-            //  bp.messenger.sendText(event.user.id, lyrics);
+            let text = item.lyrics.split(',');
+            let random = Math.floor((Math.random() * (20 - 2)) + 2);
+            console.log(text[random]);
+            bp.messenger.sendText(event.user.id, text[random]);
+            return null;
         });
 
         })
@@ -21,4 +25,4 @@ module.exports = function(bp) {
     bp.hear('alex', event => {
         bp.messenger.sendText(event.user.id, "Alex is the man");
     })
-}
+};
