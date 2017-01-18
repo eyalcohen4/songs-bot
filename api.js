175e945ff9908135e1acd2d1b7ead9fc6903fecf
eyalcohen4 <Eyalcohen4.ec@gmail.com>
2017-01-18 21:35:01 +0200

    Handle not understend situation

--- api.js
+++ api.js
@@ -14,4 +14,4 @@ function sendRequest(url, resolve, reject) {
 
 module.exports = {
     sendRequest: sendRequest,
-}
+};
--- index.js
+++ index.js
@@ -1,32 +1,44 @@
 const api       = require('./api');
 const encodeUrl = require('encodeurl');
 
+const options = {
+    typing:   true,
+    waitRead: true,
+};
+
+function getByNameSuccess() {
+
+}
+
+function getByNameError() {
+
+}
+
 module.exports = function(bp) {
   bp.middlewares.load();
 
-   bp.hear(/[\u0590-\u05FF]/, event => {
-    let term = encodeUrl(event.text);
-    console.log(term);
-    api.sendRequest(`http://localhost:3001/api/songs/name/${term}`).then(data => {
-        console.log(data);
-        data = JSON.parse(data);
-
-        data.map(function (item) {
-            let text = item.lyrics.split(',' || '\n');
-            let random = Math.floor((Math.random() * (text.length - 2)) + 2);
-
-            if (text[random].length >= 300) {
-                text[random] = text[random].slice(1, 299);
+   bp.hear(/[\u0590-\u05FF]/, (event, next) => {
+       let term = encodeUrl(event.text);
+        api.sendRequest(`http://localhost:3001/api/songs/name/${term}`).then(data => {
+            data = JSON.parse(data);
+            if (data.length === 0) {
+                bp.messenger.sendText(event.user.id, `${event.user.first_name} ${event.user.last_name}, אני באמת לא מבין אותך`);
             }
+            data.map(function (item) {
+                let text = item.lyrics.split(',' || '\n');
+                let random = Math.floor((Math.random() * (text.length - 2)) + 2);
 
-            bp.messenger.sendText(event.user.id, text[random]);
-            return null;
-        });
+                text = (text[random] >= 300) ? text[random].slice(1, 299) : text[random];
 
-        })
+                bp.messenger.sendText(event.user.id, `${text} \n תמשיך אותי ${event.user.first_name}`);
+            });
+        }, error => {
+            console.log(error);
+            bp.messenger.sendText(event.user.id, `${event.user.first_name} ${event.user.last_name}, אני באמת לא מבין אותך`);
+       })
     });
 
     bp.hear('alex', event => {
         bp.messenger.sendText(event.user.id, "Alex is the man");
     })
-};
+    };
